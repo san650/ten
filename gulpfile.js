2a79dab7b36f706e3e4580848c844c6b16e64faa
Santiago Ferreira <san650@gmail.com>
2015-06-30 23:07:20 -0300

    Add gulp build and serve tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,19 +4,41 @@ var gulp = require('gulp'),
     iif = require('gulp-if'),
     sass = require('gulp-sass'),
     cssbeautify = require('gulp-cssbeautify'),
-    del = require('del');
+    del = require('del'),
+    browserSync = require('browser-sync');
 
-gulp.task('build', ['clean'], function() {
-  return gulp.src('apps/**')
+// Process app templates folder
+gulp.task('buildTemplates', ['clean'], function() {
+  return gulp
+    .src('apps/**')
     .pipe(iif('*.scss', sass()))
     .pipe(iif('*.css', cssbeautify({ indent: '  ' })))
     .pipe(gulp.dest('dist'));
 });
 
+// Process public folder
+gulp.task('buildPublic', ['clean'], function() {
+  return gulp
+    .src('public/**')
+    .pipe(gulp.dest('dist'));
+});
+
+// Generate all files
+gulp.task('build', ['buildTemplates', 'buildPublic']);
+
+// Clean dist folder
 gulp.task('clean', function(cb) {
   del('dist', cb);
 });
 
-gulp.task('default', function() {
-  gulp.start('build');
+// Watch files for changes & reload
+gulp.task('serve', ['build'], function () {
+  browserSync({
+    notify: false,
+    server: ['dist']
+  });
+
+  gulp.watch(['public/*.*', 'apps/**/*.html', 'apps/**/*.{scss,css}'], ['build', browserSync.reload]);
 });
+
+gulp.task('default', ['build']);
